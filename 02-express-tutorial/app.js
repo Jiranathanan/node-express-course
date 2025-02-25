56fd3c9c2aba57e85442c9ece7d9030521f97567
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-26 05:31:44 +0700

    HTTP - App Example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,11 @@ const { readFileSync } = require('fs');
 const http = require('http');
 
 // get all files
-const homepage = readFileSync('./index.html');
+// const homepage = readFileSync('./index.html');
+const homepage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeLogo = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer( (req, res) => {
     // console.log(req.method);
@@ -20,6 +24,24 @@ const server = http.createServer( (req, res) => {
         res.write('<h1>About Page</h1>');
         res.end();
     } 
+    // styles
+    else if(url === '/styles.css') {
+        res.writeHead(200, {'content-type': 'text/css'} );
+        res.write(homeStyles);
+        res.end();
+    }
+    // logo
+    else if(url === '/logo.svg') {
+        res.writeHead(200, {'content-type': 'image/svg+xml'}); 
+        res.write(homeLogo);
+        res.end();
+    }
+    // Logic
+    else if(url === '/browser-app.js') {
+        res.writeHead(200, {'content-type': 'text/javascript'});
+        res.write(homeLogic);
+        res.end();
+    }
     // 404 Error page not found
     else {
         res.writeHead(404, {'content-type': 'text/html'}); 
