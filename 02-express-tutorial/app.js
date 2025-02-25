dd1510898f63ace23eac88109f489913b4d3f2c0
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-26 04:31:57 +0700

    HTTP - Headers & Request Object

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,29 @@
-console.log('Express Tutorial')
+const { log } = require('console');
+const http = require('http');
+
+const server = http.createServer( (req, res) => {
+    // console.log(req.method);
+    console.log(req.url);
+    const url = req.url;
+    // homepage
+    if(url === '/') {
+        res.writeHead(200, {'content-type': 'text/html'}); 
+        res.write('<h1>Hi There !!!!</h1>');
+        res.end();
+    }
+    // about page
+    else if(url === '/about') {
+        res.writeHead(200, {'content-type': 'text/html'}); 
+        res.write('<h1>About Page</h1>');
+        res.end();
+    } 
+    // 404 Error page not found
+    else {
+        res.writeHead(404, {'content-type': 'text/html'}); 
+        res.write('<h1>404 Error Page Not Found !!!!</h1>');
+        res.end();
+    }
+
+})
+
+server.listen(5000);
