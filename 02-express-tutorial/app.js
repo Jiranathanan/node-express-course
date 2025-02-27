2f540d5eadd1b7de0641250d791368793cae2184
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-27 22:33:04 +0700

    Params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,29 +1,27 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// setup static and middleware
-app.use(express.static('./public'));
+const { products } = require('./data');
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
-// })
-// adding to static
-// SSR
+app.get('/', (req, res) => {
+    res.send('<h1>Homepage</h1> <a href="/api/products">products</a>')
+});
 
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>Resource not found</h1>');
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map( (product) => {
+        const { id, name, image } = product;
+        return { id, name, image }
+    })
+
+    res.json(newProducts);
+});
+
+
+
+
+
+
+
+app.listen( 5000, () => {
+    console.log('Server is listening on port 5000....');
 })
-
-app.listen(5000, () => {
-    console.log('Server is listening on port 5000');
-})
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
-
