41093847bba049f4c25fdbe3d33e5d972bd3d562
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-28 05:26:25 +0700

    Query String

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.send('<h1>Homepage</h1> <a href="/api/products">products</a>')
 });
 
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map( (product) => {
+        const { id, name, image } = product;
+        return { id, name, image}
+    })
+    res.json(newProducts);
+})
+
 app.get('/api/products/:productID', (req, res) => {
     const { productID } = req.params;
     const singleProduct = products.find(
@@ -20,7 +28,26 @@ app.get('/api/products/:productID', (req, res) => {
     return res.json(singleProduct);
 });
 
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query);
+    const { search, limit } = req.query;
+    let sortedProducts = [...products]
 
+    if(search) {
+        sortedProducts = sortedProducts.filter( (product) => {
+            return product.name.startsWith(search);
+        })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if(sortedProducts.length < 1) {
+        // res.status(200).send('no products matched your search');
+        return res.status(200).json({success: true, data:[sortedProducts]})
+    }
+    res.status(200).json(sortedProducts);
+    // res.send('Hello World');
+})
 
 
 
