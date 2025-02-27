77489444f48501813cace04127d5e48d237841dd
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-28 04:47:07 +0700

    Params II

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,13 +7,17 @@ app.get('/', (req, res) => {
     res.send('<h1>Homepage</h1> <a href="/api/products">products</a>')
 });
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map( (product) => {
-        const { id, name, image } = product;
-        return { id, name, image }
-    })
+app.get('/api/products/:productID', (req, res) => {
+    const { productID } = req.params;
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+    );
 
-    res.json(newProducts);
+    if(!singleProduct) {
+        return res.status(404).send('Product does not exist');
+    }
+    // console.log(singleProduct);
+    return res.json(singleProduct);
 });
 
 
