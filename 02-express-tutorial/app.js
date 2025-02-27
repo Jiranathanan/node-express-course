a52b0171d496c43652e1f724cddd5dfa01d9637b
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-02-27 11:31:43 +0700

    Express - App Example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('<h1>Homepage</h1>');
-})
+// setup static and middleware
+app.use(express.static('./public'));
 
-app.get('/about', (req, res) => {
-    res.status(200).send('<h1>About</h1>');
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
 })
 
 app.all('*', (req, res) => {
-    res.status(404).send('<h1>Resource not found</h1>')
+    res.status(404).send('<h1>Resource not found</h1>');
 })
 
 app.listen(5000, () => {
